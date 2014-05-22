28a0fd6ebd2641241b81a95d8b50972bba3d591e
David Ellis <david.ellis@rackspace.com>
2014-05-22 17:05:13 -0500

    initial commit

---
+++ tradesim/runner.fs
@@ -0,0 +1,70 @@
+﻿module dke.tradesim.Runner
+
+open CommandLine
+open CommandLine.Text
+open System
+
+// command line parser example found at
+// http://social.msdn.microsoft.com/Forums/en-US/9e858a99-19f4-437e-b157-57ec548f2e7b/using-commandline-library-from-f?forum=fsharpgeneral
+type Opt = CommandLine.OptionAttribute
+type RuntimeConfig() =
+  class
+    [<Opt('d', "db", HelpText = "Database connection string")>]
+    member val ConnectionString = "" with get, set
+
+    [<Opt('u', "username", HelpText = "Database username")>]
+    member val Username = "" with get, set
+
+    [<Opt('p', "password", HelpText = "Database password")>]
+    member val Password = "" with get, set
+
+    [<Opt("build-trial-samples", HelpText = "Build missing trial samples")>]
+    member val BuildTrialSamples = false with get, set
+
+    [<Opt('s', "scenario", HelpText = "Run scenario <i>")>]
+    member val Scenario = null with get, set
+
+    [<HelpOption>]
+    member this.GetUsage() = 
+      HelpText.AutoBuild(
+        this, 
+        fun current -> HelpText.DefaultParsingErrorsHandler(this, current)
+      ).ToString() +
+      "Usage:\n\
+        tradesim [--db jdbc:postgresql://localhost[:port]/tradesim] [--username <username>] [--password <password>] [--build-trial-samples | --scenario <scenarioName>]\n\n\
+      Example:\n\
+        tradesim --scenario buyandhold1\n"
+  end
+
+let parseCommandLineArgs args =
+  // command line parser example found at
+  // http://social.msdn.microsoft.com/Forums/en-US/9e858a99-19f4-437e-b157-57ec548f2e7b/using-commandline-library-from-f?forum=fsharpgeneral
+  let runtimeConfig = new RuntimeConfig()
+  if Parser.Default.ParseArguments(args, runtimeConfig) then
+    Some(runtimeConfig)
+  else
+    None
+
+
+[<EntryPoint>]
+let main argv = 
+  let parsedOptions = parseCommandLineArgs argv
+  parsedOptions |> Option.map (fun options ->
+    if options.BuildTrialSamples then
+      printfn "build trial samples"
+    elif options.Scenario <> null then
+      printfn "run scenario %A" options.Scenario
+    ) |> ignore
+
+  0
+
+
+
+
+
+
+
+
+
+
+
---
+++ tradesim/typeclass.fs
@@ -0,0 +1,42 @@
+﻿module dke.tradesim.Typeclass
+
+// typeclass
+type AddSubtract<'t> = {
+  add: 't -> 't -> 't
+  subtract: 't -> 't -> 't
+}
+
+// user defined type
+type Pair = int * int   // Pair is a 2-tuple
+
+// instance of the type class defined on user defined type
+let AddSubtractPair = {
+  add = fun (a, b) (x, y) -> a + x, b + y
+  subtract = fun (a, b) (x, y) -> a - x, b - y
+}
+
+let AddSubtractString = {
+  add = fun (a: string) b -> a + b            // I'm defining add to do string concatenation
+  subtract = fun a b -> a.Replace(b, "")      // I'm defining subtract to remove any occurance of b in a with ""
+}
+
+// helper functions to make it more natural to use functions defined in type class
+let add tc a b = tc.add a b
+let subtract tc a b = tc.subtract a b
+
+let testAddSubtractTypeclass () =
+  let a = (5, 6): Pair
+  let b = (4, 10)
+
+  let x = "abcdefg"
+  let y = "cde"
+
+  printfn "%A" a
+  printfn "%A" b
+  printfn "%A" (add AddSubtractPair a b)
+  printfn "%A" (subtract AddSubtractPair a b)
+
+  printfn "%A" x
+  printfn "%A" y
+  printfn "%A" (add AddSubtractString x y)
+  printfn "%A" (subtract AddSubtractString x y)

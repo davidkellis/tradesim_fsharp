8365bc56bb0fd62e9a99a18a62b105fa80196d97
David Ellis <david.ellis@rackspace.com>
2014-07-01 17:11:00 -0500

    adding AdjustedQuotes module

---
+++ tradesim/adjustedQuotes.fs
@@ -0,0 +1,40 @@
+﻿module dke.tradesim.AdjustedQuotes
+
+open NodaTime
+
+open Core
+open Quotes
+open CorporateActions
+
+let adjEodQuote (time: ZonedDateTime) (securityId: SecurityId) (priceFn: Bar -> decimal) dao: Option<decimal> =
+  let bar = findEodBar time securityId dao
+  Option.map 
+    (fun bar ->
+      let price = priceFn bar
+      let priceObservationTime = bar.endTime
+      adjustPriceForCorporateActions price securityId priceObservationTime time dao
+    )
+    bar
+
+let adjEodClose (time: ZonedDateTime) (securityId: SecurityId) dao: Option<decimal> = adjEodQuote time securityId (fun bar -> bar.c) dao
+
+let adjEodOpen (time: ZonedDateTime) (securityId: SecurityId) dao: Option<decimal> = adjEodQuote time securityId (fun bar -> bar.o) dao
+
+let adjEodSimQuote (time: ZonedDateTime) (securityId: SecurityId) dao: Option<decimal> = adjEodQuote time securityId barSimQuote dao
+
+
+let adjEodQuotePriorTo (time: ZonedDateTime) (securityId: SecurityId) (priceFn: Bar -> decimal) dao: Option<decimal> =
+  let bar = findEodBarPriorTo time securityId dao
+  Option.map
+    (fun bar ->
+      let price = priceFn bar
+      let priceObservationTime = bar.endTime
+      adjustPriceForCorporateActions price securityId priceObservationTime time dao
+    )
+    bar
+
+let adjEodClosePriorTo (time: ZonedDateTime) (securityId: SecurityId) dao: Option<decimal> = adjEodQuotePriorTo time securityId (fun bar -> bar.c) dao
+
+let adjEodOpenPriorTo (time: ZonedDateTime) (securityId: SecurityId) dao: Option<decimal> = adjEodQuotePriorTo time securityId (fun bar -> bar.o) dao
+
+let adjEodSimQuotePriorTo (time: ZonedDateTime) (securityId: SecurityId) dao: Option<decimal> = adjEodQuotePriorTo time securityId barSimQuote dao

45ea06037a2160badb06dc8e24f59bf36156ae35
David Ellis <davidkellis@gmail.com>
2014-07-04 00:26:11 -0500

    adding more logic to Math module

--- tradesim/math.fs
+++ tradesim/math.fs
@@ -10,24 +10,24 @@ let integralQuotient (dividend: decimal) (divisor: decimal): decimal = floor (di
 
 module Decimal = 
 
-  let billionth = 0.000000001M
+  let billionth = 0.000000001m
 
   let sum (xs: seq<decimal>): decimal = Seq.reduce (+) xs
 
-  let isSignSame a b = (a < 0M) = (b < 0M)
+  let isSignSame a b = (a < 0m) = (b < 0m)
 
   let isSignDiff a b = not (isSignSame a b)
 
   let rec pow (x: decimal) (exp: int): decimal =
     if exp > 0 then
-      let mutable acc = 1M
+      let mutable acc = 1m
       for i = 1 to exp do
         acc <- acc * x
       acc
     elif exp = 0 then
-      1M
+      1m
     else
-      1M / pow x -exp
+      1m / pow x -exp
 
   // This is a recursive implementation of the Bisection Method as defined by pseudocode in
   // Bisection3 on page 95 of Numerical Mathematics and Computing (5th ed.) by Ward/Kincaid.
@@ -42,7 +42,7 @@ module Decimal =
   // Returns:
   // [root-approximation error number-of-iterations]
   let rec findRootWithBisectionMethodR (f: decimal -> decimal) (a: decimal) (b: decimal) (fa: decimal) (fb: decimal) (maxN: int) (epsilon: decimal) (n: int): decimal =
-    let error = (b - a) / 2M     // error <- (b - a) / 2
+    let error = (b - a) / 2m     // error <- (b - a) / 2
     let c = a + error            // c <- a + error  (c is the midpoint between a and b ; this is our best root approximation)
     let fc = (f c)               // fc <- f(c)      (fc is f evaluated at the midpoint between a and b)
     let n' = n + 1
@@ -80,18 +80,34 @@ module Decimal =
   //   -> 0.456
   let nthRoot (a: decimal) (n: int): decimal =
     let f = (fun x -> (pow x n) - a)
-    if a < 1M then
-      findRootWithBisectionMethod f 0M 1M
+    if a < 1m then
+      findRootWithBisectionMethod f 0m 1m
     else
-      findRootWithBisectionMethod f 0M a
+      findRootWithBisectionMethod f 0m a
+  
+  let sqrt n = nthRoot n 2
 
+  // Approximate the definite integral of f from a to b, using Composite Simpson's rule: http://en.wikipedia.org/wiki/Simpson's_rule#Composite_Simpson.27s_rule
+  // This implementation implements:
+  //   integral from a to b of f(x) dx is approximated by
+  //   h/3 * [ f(x_0) + 4f(x_1) + 2f(x_2) + 4f(x_3) + 2f(x_4) + ... + 4f(x_n-1) + f(x_n) ]
+  //   where h = (b-a)/n and x_i = a + i*h for i = 0, 1, ..., n-1, n; x_0 = a + 0*h = a; x_n = a + n*h = a + n((b-a)/n) = b
+  // NOTE: numberOfSubintervalsN must be even!!!
+  // Example:
+  //   On Wolfram Alpha, evaluate: Simpson's rule 2+1/(sqrt(x))+1/(4*x) on [2,5] with interval size 0.5:
+  //
+  //   let f (x: decimal) = 2m + (1m / sqrt x) + (1m / (4m * x))
+  //   integrateWithCompositeSimpsonsRule f 2m 5m 6
+  //   val it : decimal = 7.8728590394739751961083936282M
+  //
+  //   On Wolfram Alpha, evaluate: integral (x^2-2)/x dx from 1 to 2 using Boole's rule:
+  //
+  //   let f (x: decimal) = (pow x 2 - 2m) / x
+  //   integrateWithCompositeSimpsonsRule f 1m 2m 6
+  //   val it : decimal = 0.1136604136604136604136604139M
+  let integrateWithCompositeSimpsonsRule (f: decimal -> decimal) (a: decimal) (b: decimal) (numberOfSubintervalsN: int): decimal =
+    let h = ((b - a) / decimal numberOfSubintervalsN)       // factor this out of the loop for efficiency
+    let summation = seq { 1 .. (numberOfSubintervalsN - 1) } |> Seq.map (fun k -> (decimal ((k % 2) + 1)) * f (a + (h * decimal k))) |> sum
+    (h / 3m) * ((f a) + (2m * summation) + (f b))
 
-  // todo: finish implementing this based on https://github.com/davidkellis/dke-contrib/blob/master/dke/contrib/math.clj
-  // Approximate the definite integral of f from a to b, using Composite Simpson's rule
-  // This is a Clojure port of the Python implementation at http://en.wikipedia.org/wiki/Simpson's_rule
-  let integrateWithSimpsonsRuleR f a b n: decimal =
-    let h = ((b - a) / n)       // factor this out of the loop for efficiency
-    let sum = seq { 1 .. n } |> Seq.map (fun k -> ((k % 2) + 1) * (f ( a + (h * k)))) |> sum
-    (h / 3) * ((f a) + (2 * sum) + (f b))
-
-  let integrateWithSimpsonsRule f a b = integrateWithSimpsonsRuleR f a b 100
+  let integrateWithSimpsonsRule f a b = integrateWithCompositeSimpsonsRule f a b 100

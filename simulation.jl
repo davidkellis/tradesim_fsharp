49f7b20317bfec3e5f55fe85d6d773b8581409a2
David Ellis <davidkellis@gmail.com>
2015-02-19 23:20:55 -0600

    more work on simulation.js

--- simulation.jl
+++ simulation.jl
@@ -28,34 +28,36 @@ end
 
 function print_dist_stats(distribution, expected_mean)
   # compute statistics of sampling distribution
-  q005, q025, q5, q975, q995 = round(quantile(distribution, [0.005, 0.025, 0.5, 0.975, 0.995]), 4)
+  q005, q5, q995 = round(quantile(distribution, [0.005, 0.5, 0.995]), 4)
   mean_of_samp_dist = round(mean(distribution), 4)
   std_of_samp_dist = round(std(distribution), 4)
-  iqr_of_samp_dist = round(iqr(distribution), 4)
+  range_of_99th_percentile_of_samp_dist = round(abs(q995 - q005), 4)
   
   is_accurate = q005 <= expected_mean <= q995
-  println("accurate=$is_accurate   mean=$mean_of_samp_dist   std=$std_of_samp_dist   iqr=$iqr_of_samp_dist   $q005 --- $q025 --- $q5 --- $q975 --- $q995")
+  println("accurate=$is_accurate   mean=$mean_of_samp_dist   std=$std_of_samp_dist   99th %-ile range=$range_of_99th_percentile_of_samp_dist   $q005 --- $q5 --- $q995")
 end
 
 # compute sampling distribution of simulated annual returns
 function main()
-  # sigma = 1.4 ^ (1/52) - 1
-  sigma = 1.4 ^ (1/251) - 1
-  return_dist = Normal(1, sigma)                             # mu = 1; sigma = 1.4 ^ (1/52) - 1    # sigma of [1.4 ^ (1/52) - 1] simulates a weekly return that when annualized represents a +-40% annual gain 68% of the time and +-80% annual gain 95% of the time
+  n_periods_per_year = 52
+  sigma = 1.4 ^ (1/n_periods_per_year) - 1
+  # sigma = (1 + 0.82) ^ (1/251) - 1   # estimate of sigma taken from http://blog.iese.edu/jestrada/files/2012/06/DSRSSM.pdf
+  return_dist = Normal(1, sigma)     # mu = 1; sigma = 1.4 ^ (1/52) - 1    # sigma of [1.4 ^ (1/52) - 1] simulates a weekly return that when annualized represents a +-40% annual gain 68% of the time and +-80% annual gain 95% of the time
 
   for i in 1:100
     # construct original sample of observations
-    n_return_observations = 251
-    return_observations = rand(return_dist, n_return_observations)            # create sample of return observations
+    n_return_observations = 26 * i
+    return_observations = max(rand(return_dist, n_return_observations), 0)            # create sample of return observations; all values are >= 0
 
-    n_annualized_returns = 1000
-    n_periods = 251
-    n_samples = 1000
+    println("$i - $n_return_observations return observations")
+
+    n_annualized_returns = 50000
+    n_samples = 5000
     n_observations_per_sample = 1000
     
-    # annualized_returns = build_bootstrap_distribution(return_observations, n_annualized_returns, (sample) -> prod(sample) ^ (52/n_periods), n_periods)    # for weekly returns
-    # annualized_returns = build_bootstrap_distribution(return_observations, n_annualized_returns, (sample) -> prod(sample) ^ (251/n_periods), n_periods)    # for daily returns
-    annualized_returns = build_bootstrap_distribution(return_observations, n_annualized_returns, (sample) -> prod(sample), n_periods)    # for daily returns
+    # annualized_returns = build_bootstrap_distribution(return_observations, n_annualized_returns, (sample) -> prod(sample) ^ (52/n_periods_per_year), n_periods_per_year)    # for weekly returns
+    # annualized_returns = build_bootstrap_distribution(return_observations, n_annualized_returns, (sample) -> prod(sample) ^ (251/n_periods_per_year), n_periods_per_year)    # for daily returns
+    annualized_returns = build_bootstrap_distribution(return_observations, n_annualized_returns, (sample) -> prod(sample), n_periods_per_year)
     
     print_dist_stats(annualized_returns, 1)
 
@@ -73,11 +75,11 @@ function main2()
 
   for i in 1:100
     # construct original sample of observations
-    n_return_observations = 1000
-    return_observations = rand(return_dist, n_return_observations)            # create sample of return observations
+    n_return_observations = 100
+    return_observations = max(rand(return_dist, n_return_observations), 0)            # create sample of return observations; all values are >= 0
 
     n_samples = 5000
-    n_observations_per_sample = 100
+    # n_observations_per_sample = 100
     
     # samp_dist = build_bootstrap_distribution(return_observations, n_samples, mean, n_observations_per_sample)
     samp_dist = build_bootstrap_distribution(return_observations, n_samples, mean)

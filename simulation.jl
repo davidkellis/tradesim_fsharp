5b56cbd70966e045ffcfd552bc9bd60b2d802892
David Ellis <david.ellis@rackspace.com>
2015-02-20 17:41:33 -0600

    simulation.js

--- simulation.jl
+++ simulation.jl
@@ -39,19 +39,19 @@ end
 
 # compute sampling distribution of simulated annual returns
 function main()
-  n_periods_per_year = 52
+  n_periods_per_year = 251
   sigma = 1.4 ^ (1/n_periods_per_year) - 1
   # sigma = (1 + 0.82) ^ (1/251) - 1   # estimate of sigma taken from http://blog.iese.edu/jestrada/files/2012/06/DSRSSM.pdf
   return_dist = Normal(1, sigma)     # mu = 1; sigma = 1.4 ^ (1/52) - 1    # sigma of [1.4 ^ (1/52) - 1] simulates a weekly return that when annualized represents a +-40% annual gain 68% of the time and +-80% annual gain 95% of the time
 
   for i in 1:100
     # construct original sample of observations
-    n_return_observations = 26 * i
+    n_return_observations = 63*i
     return_observations = max(rand(return_dist, n_return_observations), 0)            # create sample of return observations; all values are >= 0
 
     println("$i - $n_return_observations return observations")
 
-    n_annualized_returns = 50000
+    n_annualized_returns = n_return_observations    # 1000000
     n_samples = 5000
     n_observations_per_sample = 1000
     
